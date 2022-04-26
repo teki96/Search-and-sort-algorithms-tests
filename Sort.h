ab6972bfe5cb4705a8a8ddfdf2cc159f7176f34d
teki96 <teemu.t.k101@gmail.com>
2022-04-26 18:50:22 +0300

    Added changes to insertion sort to take list as parameter

--- Sort.h
+++ Sort.h
@@ -4,7 +4,7 @@ template <class Record>
 class Sortable_list :public List<Record> {
 public:
 	Sortable_list();
-	void insertion_sort();
+	void insertion_sort(List<Record>& list);
 private:
 
 };
@@ -15,7 +15,7 @@ inline Sortable_list<Record>::Sortable_list()
 }
 
 template <class Record>
-void Sortable_list<Record>::insertion_sort()
+void Sortable_list<Record>::insertion_sort(List<Record>& list)
 /*
 Post: The entries of the Sortable_list have been rearranged so that
       the keys in all the  entries are sorted into nondecreasing order.
@@ -27,15 +27,15 @@ Uses: Methods for the class Record; the contiguous List implementation of
     int position;          //  searches sorted part of list
     Record current;        //  holds the entry temporarily removed from list
 
-    for (first_unsorted = 1; first_unsorted < count; first_unsorted++)
-        if (entry[first_unsorted] < entry[first_unsorted - 1]) {
+    for (first_unsorted = 1; first_unsorted < list.count; first_unsorted++)
+        if (list.entry[first_unsorted] < list.entry[first_unsorted - 1]) {
             position = first_unsorted;
-            current = entry[first_unsorted];         //  Pull unsorted entry out of the list.
+            current = list.entry[first_unsorted];         //  Pull unsorted entry out of the list.
             do {               //  Shift all entries until the proper position is found.
-                entry[position] = entry[position - 1];
+                list.entry[position] = list.entry[position - 1];
                 position--;                           //  position is empty.
-            } while (position > 0 && entry[position - 1] > current);
-            entry[position] = current;
+            } while (position > 0 && list.entry[position - 1] > current);
+            list.entry[position] = current;
         }
 }
 
